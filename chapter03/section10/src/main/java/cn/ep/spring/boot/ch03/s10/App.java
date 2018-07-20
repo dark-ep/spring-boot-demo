784583710ebb336083761ab68446b38bcb7803c3
lihongle <lhl_1986@qq.com>
2018-07-20 22:21:45 +0800

    feat:03.10.使用AMQP操作RabbitMQ

---
+++ chapter03/section10/src/main/java/cn/ep/spring/boot/ch03/s10/App.java
@@ -0,0 +1,13 @@
+package cn.ep.spring.boot.ch03.s10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class).close();
+    }
+
+}
---
+++ chapter03/section10/src/main/java/cn/ep/spring/boot/ch03/s10/RabbitMQConfig.java
@@ -0,0 +1,45 @@
+package cn.ep.spring.boot.ch03.s10;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String TOPIC_EXCHANGE_NAME = "spring-boot-exchange";
+
+    public static final String QUEUE_NAME = "spring-boot";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(TOPIC_EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+}
