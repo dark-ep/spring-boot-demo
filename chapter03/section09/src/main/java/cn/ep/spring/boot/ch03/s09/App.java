af56606307dbc9dad3a6d8a456a9a05895c65a2f
lihongle <lhl_1986@qq.com>
2018-07-20 22:20:42 +0800

    feat:03.09.使用LDAP查询数据

--- chapter03/section09/src/main/java/cn/ep/spring/boot/ch03/s09/App.java
+++ chapter03/section09/src/main/java/cn/ep/spring/boot/ch03/s09/App.java
@@ -15,7 +15,7 @@ public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(App.class, args).close();
     }
 
     @Bean
