ed237f0fb935c5b3474bbfc0c5ae9bf4e6865a73
lihongle <lhl_1986@qq.com>
2018-07-30 22:39:22 +0800

    feat:05.03.进行Web页面测试

---
+++ chapter05/section03/src/test/java/cn/ep/spring/boot/ch05/s03/test/CustomTest.java
@@ -0,0 +1,26 @@
+package cn.ep.spring.boot.ch05.s03.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class CustomTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testHtmlBody() {
+        String body = this.restTemplate.getForObject("/", String.class);
+        assertThat(body).isEqualTo("Hello World!");
+    }
+
+}
